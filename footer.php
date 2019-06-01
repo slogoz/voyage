a77a4c8800a14d58dc45107e5f7b6dea9a13fd43
slogoz <slogoz@yandex.ru>
2019-06-01 19:20:55 +0500

    underscores custom footer

--- footer.php
+++ footer.php
@@ -15,16 +15,16 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'wfvoyage' ) ); ?>">
+			<a href="<?php echo esc_url( __( 'http://autoex.me/', 'wfvoyage' ) ); ?>">
 				<?php
 				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'wfvoyage' ), 'WordPress' );
+				printf( esc_html__( 'Модульная среда проектирования %s', 'wfvoyage' ), 'WebFactory' );
 				?>
 			</a>
 			<span class="sep"> | </span>
 				<?php
 				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'wfvoyage' ), 'wfvoyage', '<a href="http://underscores.me/">Underscores.me</a>' );
+				printf( esc_html__( 'Тема: %1$s  %2$s.', 'wfvoyage' ), '<a href="http://autoex.me/">WebFactory</a>', 'Voyage' );
 				?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
